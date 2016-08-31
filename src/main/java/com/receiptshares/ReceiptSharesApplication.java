d27aa9e7792163bc748e3692ef51b3fb3577900a
Ivan Tatsenko <za-box@mail.ru>
2016-09-01 00:21:21 +0300

    Made react work

--- src/main/java/com/receiptshares/ReceiptSharesApplication.java
+++ src/main/java/com/receiptshares/ReceiptSharesApplication.java
@@ -2,13 +2,18 @@ package com.receiptshares;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-@EnableWebMvc
-public class ReceiptSharesApplication {
+public class ReceiptSharesApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReceiptSharesApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ReceiptSharesApplication.class);
 	}
 }
