69b9873b16a5bf03bd1a9805cc69702fc91543fb
Ivan Tatsenko <ivan.tatsenko@gmail.com>
2016-09-07 14:30:49 +0300

    Captcha secret to props

--- src/main/groovy/com/receiptshares/user/registration/GoogleCaptcha.groovy
+++ src/main/groovy/com/receiptshares/user/registration/GoogleCaptcha.groovy
@@ -2,12 +2,18 @@ package com.receiptshares.user.registration
 
 import groovy.transform.CompileStatic
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
 
+@Component
 class GoogleCaptcha {
 
     private static final String URL = "https://www.google.com/recaptcha/api/siteverify"
     RestTemplate restTemplate
+
+    @Value("${service.captcha.secret}")
+    private String secret
 
     @Autowired
     GoogleCaptcha(RestTemplate restTemplate) {
@@ -16,7 +22,7 @@ class GoogleCaptcha {
 
     def verify(String token, def ip) {
         CaptchaResponse result = restTemplate.postForObject(URL,
-                new Expando(secret: '6Lc2gykTAAAAAJTbDu344PmUi3VLAz3gh7uyTPv-', response: token), CaptchaResponse)
+                new Expando(secret: secret, response: token), CaptchaResponse)
         return result.success
     }
 }
