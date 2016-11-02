6786768be5c089c11b4e6127b4b337f63d75768f
Ivan Tatsenko <ivan.tatsenko@gmail.com>
2016-11-02 19:21:14 +0200

    Floating action button

--- src/main/resources/static/js/components/main.jsx
+++ src/main/resources/static/js/components/main.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {hashHistory} from 'react-router'
-import Header from './header/header.jsx'
-import RegistrationForm from './registration-form.jsx'
-import LoginForm from './login/login-form.jsx'
-import LeftMenu from './left-menu.jsx'
-import Receipt from './receipt.jsx'
-import Paper from 'material-ui/Paper';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import {hashHistory} from "react-router";
+import Header from "./header/header.jsx";
+import RegistrationForm from "./registration-form.jsx";
+import LoginForm from "./login/login-form.jsx";
+import LeftMenu from "./left-menu.jsx";
+import Receipt from "./receipt.jsx";
+import Paper from "material-ui/Paper";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import FloatingActionButton from "material-ui/FloatingActionButton";
+import ContentAdd from "material-ui/svg-icons/content/add";
+import IconMenu from "material-ui/IconMenu";
+import MenuItem from "material-ui/MenuItem";
+import injectTapEventPlugin from "react-tap-event-plugin";
 var ReactRouter = require('react-router');
+
+injectTapEventPlugin();
 
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
@@ -35,22 +41,44 @@ class MainPage extends React.Component {
 
     render() {
         return (
-            <div style={{height: '100%'}}>
-                <Header user={this.props.user}/>
-                <div style={{
-                    float: 'left'
-                }} className="container-div">
-                    <LeftMenu currentLink="/current" helpLink="/help"/>
+            <Mui>
+                <div style={{height: '100%'}} className="clearfix">
+                    <Header user={this.props.user}/>
+                    <div style={{
+                        float: 'left'
+                    }} className="container-div">
+                        <LeftMenu currentLink="/current" helpLink="/help"/>
+                    </div>
+                    <div style={{
+                        float: 'left',
+                        marginLeft: '20px',
+                        marginTop: '20px',
+                        width: 'auto'
+                    }} className="container-div">
+                        {this.renderChildren()}
+                    </div>
                 </div>
-                <div style={{
-                    float: 'left',
-                    marginLeft: '20px',
-                    marginTop: '20px',
-                    width: 'auto'
-                }} className="container-div">
-                    {this.renderChildren()}
-                </div>
-            </div>
+                <IconMenu
+                    style={{
+                        position: 'absolute',
+                        right: '30px',
+                        bottom: '20px'
+                    }}
+                    iconButtonElement={
+                        <FloatingActionButton zDepth={3}>
+                            <ContentAdd/>
+                        </FloatingActionButton>
+                    }
+                    onItemTouchTap={(event, item)=> {
+                        console.log(item);
+                        hashHistory.push('/current')
+                    }}
+                    anchorOrigin={{horizontal: 'left', vertical: 'top'}}
+                    targetOrigin={{horizontal: 'middle', vertical: 'bottom'}}>
+                    <MenuItem primaryText="New receipt"/>
+                </IconMenu>
+
+            </Mui>
         )
     }
 
@@ -81,7 +109,6 @@ class Mui extends React.Component {
                 <div className="container">
                     <div className="col-lg-6 col-lg-offset-3">
                         <Paper zDepth={3}>
-
                             {this.props.children}
                         </Paper>
                     </div>
@@ -103,10 +130,8 @@ function getMainLayout(user) {
     return <Router history={hashHistory}>
         <Route path="/" component={mainPageWrapper}>
             <IndexRoute component={WelcomePage}/>
-            <Route path="/register" component={RegistrationForm}/>
             <Route path="/current" component={Receipt}/>
             <Route path="/help" component={Help}/>
-            <Route path="/login" component={LoginForm}/>
             <Route path="*" component={RedirectComponent}/>
         </Route>
     </Router>;
