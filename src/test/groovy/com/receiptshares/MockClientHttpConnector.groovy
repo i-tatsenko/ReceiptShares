c1b8454adcdb14f5c84013e4a26b00623ce74953
Ivan Tatsenko <ivan.tatsenko@gmail.com>
2017-04-21 11:38:51 +0300

    Making tests green

--- src/test/groovy/com/receiptshares/MockClientHttpConnector.groovy
+++ src/test/groovy/com/receiptshares/MockClientHttpConnector.groovy
@@ -1,7 +1,9 @@
 package com.receiptshares
 
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.client.reactive.ClientHttpConnector
 import org.springframework.http.client.reactive.ClientHttpRequest
 import org.springframework.http.client.reactive.ClientHttpResponse
@@ -9,6 +11,8 @@ import org.springframework.mock.http.client.reactive.MockClientHttpResponse
 import reactor.core.publisher.Mono
 
 import java.util.function.Function
+
+import static org.springframework.http.MediaType.APPLICATION_JSON
 
 class MockClientHttpConnector implements ClientHttpConnector {
 
@@ -21,13 +25,32 @@ class MockClientHttpConnector implements ClientHttpConnector {
         if (!result) {
             throw new IllegalArgumentException("There is no mapping for ${uri.toString()}")
         }
-        def response = new MockClientHttpResponse(HttpStatus.OK)
+        def response = new MockResponse(HttpStatus.OK)
         response.setBody(result)
+        response.setContentType(APPLICATION_JSON)
         return Mono.just(response)
     }
 
     def mockPost(String uri, String response) {
         stubs.computeIfAbsent(HttpMethod.POST, {method-> [:]})
              .put(uri, response)
+    }
+
+    static class MockResponse extends MockClientHttpResponse {
+
+        private HttpHeaders headers = new HttpHeaders()
+
+        MockResponse(HttpStatus status) {
+            super(status)
+        }
+
+        def setContentType(MediaType contentType) {
+            headers.setContentType(contentType)
+        }
+
+        @Override
+        HttpHeaders getHeaders() {
+            return headers
+        }
     }
 }
